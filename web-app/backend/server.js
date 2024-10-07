060236a7ef60ab8801470d9c6f6411cb86c1fd87
Abhirup Bhattacharya <abhirup706@gmail.com>
2024-10-06 22:15:37 -0500

    added journal routes in server.js

--- web-app/backend/server.js
+++ web-app/backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const uploadRoute = require('./routes/uploads');
+const journalRoute = require('./routes/journal');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use('/profileai', uploadRoute);
+app.use('/profileai', journalRoute);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
